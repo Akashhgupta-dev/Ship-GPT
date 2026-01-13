71eaad0359e838e4a0799f2c1a3df25a4d2d8ac1
Akashhgupta-dev <akash.gupta.digixito@gmail.com>
2026-01-13 15:04:18 +0530

    change baseurl

--- api/server-constant.js
+++ api/server-constant.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://13.49.158.124/";
+const baseUrl = "https://13.49.158.124/";
 export const serverConstant = {
   authenticationUrls: `${baseUrl}api/auth`,
   chatUrls: `${baseUrl}api/`,
